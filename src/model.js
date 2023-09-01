8a1d537bcc6ce0f387b32478515d8c144e8e84a8
Priscila Schleske <plasch7@gmail.com>
2023-09-01 12:18:37 -0600

    completes model including relationship link and getName

--- src/model.js
+++ src/model.js
@@ -9,21 +9,71 @@ export class Human extends Model {
     return this.toJSON();
   }
 
-  getFullName() {
+  async getFullName() {
     // TODO: Implement this method
+    return `${this.fname} ${this.lname}`
   }
 }
-
 // TODO: Human.init()
+Human.init(
+  {
+    humanId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+    },
+    fname: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    lname: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    }
+  },
+  {
+    modelName: 'human',
+    sequelize: db,
+  }
+);
+
 
 export class Animal extends Model {
   [util.inspect.custom]() {
     return this.toJSON();
   }
 }
-
 // TODO: Animal.init()
-
+Animal.init(
+  {
+    animaId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    species: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    birthYear: {
+      type: DataTypes.INTEGER,
+    }
+  },
+  {
+    modelName: 'animal',
+    sequelize: db,
+  }
+);
 // TODO: Define Relationship
+Human.hasMany(Animal, { foreignKey: 'humanId' })
+Animal.belongsTo(Human, { foreignKey: 'humanId' })
 
 export default db;
